ed0ade8d7b461373cdf9cd79ff7f812adc0117fe
Nexflare <harshmagarwal@outlook.com>
2024-06-06 18:01:34 -0400

    Caddy comment :bug: fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -38,7 +38,7 @@ sudo systemctl enable docker
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
-caddy
+# caddy
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
