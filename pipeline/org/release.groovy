a6f733e502dfa0ab1f338bfae980abb2dfd5b76e
jay vekariya <vekariya.ja@northeastern.edu>
2024-06-14 11:18:41 -0400

    fix: cred name

--- pipeline/org/release.groovy
+++ pipeline/org/release.groovy
@@ -2,22 +2,22 @@ def repos = [
   [
     name: 'static-site',
     url: 'https://github.com/csye7125-su24-team05/static-site.git',
-    credentialsId: 'jenkins-status-check-app',
+    credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'webapp-migration',
     url: 'https://github.com/csye7125-su24-team05/webapp-migration.git',
-    credentialsId: 'jenkins-status-check-app',
+    credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'webapp-cve-processor',
     url: 'https://github.com/csye7125-su24-team05/webapp-cve-processor.git',
-    credentialsId: 'jenkins-status-check-app',
+    credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'helm-webapp-cve-processor',
     url: 'https://github.com/csye7125-su24-team05/helm-webapp-cve-processor.git',
-    credentialsId: 'jenkins-status-check-app',
+    credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'csye7125-test-repo',
