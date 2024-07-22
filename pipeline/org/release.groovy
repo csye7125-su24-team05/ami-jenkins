8e729bed965ca51aea70e2dbf82b82bfc3c56754
Jay Vekariya <51136830+Jay-0331@users.noreply.github.com>
2024-07-22 01:24:28 -0400

    ci: added docker release (#16)

--- pipeline/org/release.groovy
+++ pipeline/org/release.groovy
@@ -2,85 +2,90 @@ def repos = [
   [
     name: 'static-site',
     url: 'https://github.com/csye7125-su24-team05/static-site.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'webapp-migration',
     url: 'https://github.com/csye7125-su24-team05/webapp-migration.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'webapp-cve-processor',
     url: 'https://github.com/csye7125-su24-team05/webapp-cve-processor.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'helm-webapp-cve-processor',
     url: 'https://github.com/csye7125-su24-team05/helm-webapp-cve-processor.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'csye7125-test-repo',
     url: 'https://github.com/csye7125-su24-team05/csye7125-test-repo.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'webapp-cve-consumer',
     url: 'https://github.com/csye7125-su24-team05/webapp-cve-consumer.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'helm-webapp-cve-consumer',
     url: 'https://github.com/csye7125-su24-team05/helm-webapp-cve-consumer.git',
+    scriptPath: ['release'],
     credentialsId: 'github-jenkins-app',
   ],
   [
     name: 'helm-eks-autoscaler',
     url: 'https://github.com/csye7125-su24-team05/helm-eks-autoscaler.git',
+    scriptPath: ['release', 'docker-release'],
     credentialsId: 'github-jenkins-app',
   ],
-  [
-    name: 'docker-sync',
-    url: 'https://github.com/csye7125-su24-team05/docker-sync.git',
-    credentialsId: 'github-jenkins-app',
-  ]
 ]
 
 repos.each { repo ->
-  pipelineJob("${repo.name}/release") {
-    definition {
-      cpsScm {
-        scm {
-          git {
-            remote {
-              url repo.url
-              credentials repo.credentialsId
-            }
-            branches '*/main'
-            extensions {
-              localBranch()
-              cleanBeforeCheckout()
-              gitSCMChecksExtension {
-                verboseConsoleLog true
+  repo.scriptPath.each { script ->
+    pipelineJob("${repo.name}/${script}") {
+      definition {
+        cpsScm {
+          scm {
+            git {
+              remote {
+                url repo.url
+                credentials repo.credentialsId
               }
-              gitSCMStatusChecksExtension {
-                name 'release'
-                skip false
-                skipProgressUpdates true
-                suppressLogs false
-                unstableBuildNeutral false 
-              }
-              userExclusion {
-                excludedUsers 'semantic-release-bot'
+              branches '*/main'
+              extensions {
+                localBranch()
+                cleanBeforeCheckout()
+                gitSCMChecksExtension {
+                  verboseConsoleLog true
+                }
+                gitSCMStatusChecksExtension {
+                  name script
+                  skip false
+                  skipProgressUpdates true
+                  suppressLogs false
+                  unstableBuildNeutral false 
+                }
+                userExclusion {
+                  excludedUsers 'semantic-release-bot'
+                }
               }
             }
           }
+          scriptPath ".jenkins/${script}"
         }
-        scriptPath ".jenkins/release"
       }
-    }
-    triggers {
-      githubPush()
+      triggers {
+        githubPush()
+      }
     }
   }
 }
